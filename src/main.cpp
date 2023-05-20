9972136500d162cc8e711fc7669a287acdc6d1a1
Ryan Stewart <ryanstewartalex@gmail.com>
2023-05-20 07:30:27 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,41 @@
 #define INTRO_FONT FreeSansBold9pt7b
 #define INTRO_FONT_SIZE 2
 
-#define MENU_FONT FreeSans9pt7b
 #define MENU_FONT_SIZE 1
-#define MENU_SPACING 15
 #define MENU_PADDING 10
+#define MENU_SPACING 3
+
+#define VALUE_FONT_SIZE 2
+
+#define DEFAULT_EFFECT 0
+#define NUM_EFFECTS 10
+
+#define DEFAULT_PALETTE 0
+#define NUM_PALETTES 10
+
+#define DEFAULT_BRIGHTNESS 255
+#define MAX_BRIGHTNESS 255
+
+#define DEFAULT_SPEED 255
+#define MAX_SPEED 255
+
+#define DEFAULT_INTENSITY 128
+#define MAX_INTENSITY 255
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-String menuItems[] = {"EFFECT", "PALETTE", "LIGHT", "SPEED", "INTENSITY"};
+String menuItems[] = {"EFFECT", "PALETTE", "BRIGHTNESS", "SPEED", "INTENSITY"};
 int numMenuItems = 5;
 int currentMenuIndex = 0;
 bool menuItemSelected = false; // TODO:
 
+int effect = DEFAULT_EFFECT;
+int palette = DEFAULT_PALETTE;
+int brightness = DEFAULT_BRIGHTNESS;
+int speed = DEFAULT_SPEED;
+int intensity = DEFAULT_INTENSITY;
 
-int* getCenterTextCoords(String text)
+int* getCenterTextCoords(String text, bool isIntro)
 {
     int16_t x, y;
     uint16_t w, h;
@@ -33,17 +54,17 @@ int* getCenterTextCoords(String text)
 
     int* coords = new int[2];
     coords[0] = (SCREEN_WIDTH - w) / 2;
-    coords[1] = (SCREEN_HEIGHT - h) / 2;
+    coords[1] = (isIntro ? SCREEN_HEIGHT + h : SCREEN_HEIGHT - h) / 2;
     return coords;
 }
 
 void intro(String text)
 {
     display.clearDisplay();
-    // display.setFont(&INTRO_FONT);
+    display.setFont(&INTRO_FONT);
     display.setTextSize(INTRO_FONT_SIZE);
 
-    int* coords = getCenterTextCoords(text);
+    int* coords = getCenterTextCoords(text, true);
     display.setCursor(coords[0], coords[1]);
     delete[] coords;
 
@@ -64,19 +85,108 @@ void intro(String text)
 void createMenu()
 {
     display.clearDisplay();
+    display.setFont(NULL);
     display.setTextSize(MENU_FONT_SIZE);
 
-    int y = MENU_PADDING;
+    int16_t x, y;
+    uint16_t w, h;
+    display.getTextBounds(menuItems[0].c_str(), 0, 0, &x, &y, &w, &h);
+
+    int yPos = MENU_PADDING;
     for (int i = 0; i < numMenuItems; ++i)
     {
-        display.setCursor(MENU_PADDING, y);
+        display.setCursor(MENU_PADDING, yPos);
         
         String text = (i == currentMenuIndex) ? ">" + menuItems[i] : menuItems[i];
         display.print(text);
-        y += MENU_SPACING;
+        yPos += h + MENU_SPACING;
     }
 
     display.display();
+}
+
+void displayValue(int value, int maxValue)
+{
+    display.clearDisplay();
+    display.setTextSize(VALUE_FONT_SIZE);
+
+    String text = String(value) + "/" + maxValue;
+    int* coords = getCenterTextCoords(text, false);
+    display.setCursor(coords[0], coords[1]);
+    delete[] coords;
+    display.println(text);
+
+    display.display();
+}
+
+void onRotate(bool incrementing)
+{
+    if (!menuItemSelected)
+    {
+        (incrementing) ? ++currentMenuIndex : --currentMenuIndex;
+        if (currentMenuIndex == numMenuItems) 
+            currentMenuIndex = 0;
+        else if (currentMenuIndex < 0)
+            currentMenuIndex = numMenuItems - 1;
+        createMenu();
+    }
+    else 
+    {
+        switch(currentMenuIndex)
+        {
+            case 0:
+                (incrementing) ? ++effect : --effect;
+                displayValue(effect, NUM_EFFECTS);
+                break;
+            case 1:
+                (incrementing) ? ++palette : --palette;
+                displayValue(palette, NUM_PALETTES);
+                break;
+            case 2: 
+                (incrementing) ? ++brightness : --brightness;
+                displayValue(brightness, MAX_BRIGHTNESS);
+                break;
+            case 3:
+                (incrementing) ? ++speed : --speed;
+                displayValue(speed, MAX_SPEED);
+                break;
+            case 4:
+                (incrementing) ? ++intensity : --intensity;
+                displayValue(intensity, MAX_INTENSITY);
+                break;
+        }
+    }
+}
+
+void onClick()
+{
+    menuItemSelected = !menuItemSelected;
+
+    if (menuItemSelected)
+    {
+        switch(currentMenuIndex)
+        {
+            case 0:
+                displayValue(effect, NUM_EFFECTS);
+                break;
+            case 1:
+                displayValue(palette, NUM_PALETTES);
+                break;
+            case 2: 
+                displayValue(brightness, MAX_BRIGHTNESS);
+                break;
+            case 3:
+                displayValue(speed, MAX_SPEED);
+                break;
+            case 4:
+                displayValue(intensity, MAX_INTENSITY);
+                break;
+        }
+    }
+    else 
+    {
+        createMenu();
+    }
 }
 
 void setup()
@@ -98,5 +208,11 @@ void setup()
 
 void loop()
 {
-    // createMenu();
+    onRotate(true);
+    delay(1000);
+    onClick();
+    onRotate(true);
+    delay(1000);
+    onClick();
+    delay(1000);
 }
